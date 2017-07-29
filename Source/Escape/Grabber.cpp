29504528332e2f07c52c20bf8f4cc21b4fff8f04
Unknown <marcin.weglarz01@gmail.com>
2017-07-29 14:04:44 +0200

    - cleaning

--- Source/Escape/Grabber.cpp
+++ Source/Escape/Grabber.cpp
@@ -6,39 +6,23 @@
 #define OUT
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+UGrabber::UGrabber() {
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-
-    FVector PlayerViewLocation;
-    FRotator PlayerViewRotation;
-
-    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-                OUT PlayerViewLocation,
-                OUT PlayerViewRotation
-                );
-
-    FVector LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * Reach;
-
-    /// if the physics handle is attached
+    FVector LineTraceEnd = std::get<1>(GetGrabVector());
     if (PhysicsHandle->GrabbedComponent) {
         // move the object that we're holding
         PhysicsHandle->SetTargetLocation(LineTraceEnd);
     }
-
 }
 
 void UGrabber::Grab() {
-    UE_LOG(LogTemp, Warning, TEXT("Grab delegate called!"))
 
     /// Try and reach any actors with physics body collision channel set
     FHitResult Hit = GetPhysicsBodyInReach();
@@ -46,50 +30,41 @@ void UGrabber::Grab() {
     UPrimitiveComponent* ComponentToGrab = Hit.GetComponent();
     AActor* ActorHit = Hit.GetActor();
 
-    if (!ActorHit) { return; }
-
+    if (!ActorHit) { return; } // guard if ActorHit not nullptr
     // attach physics handle
     PhysicsHandle->GrabComponent(ComponentToGrab,
                                  NAME_None,
                                  ComponentToGrab->GetOwner()->GetActorLocation(),
                                  true // allow rotation
                                  );
-
 }
 
 void UGrabber::Release() {
-    UE_LOG(LogTemp, Warning, TEXT("Release delegate called!"))
     PhysicsHandle->ReleaseComponent();
 }
 
 // Called when the game starts
-void UGrabber::BeginPlay()
-{
-    Super::BeginPlay();
+void UGrabber::BeginPlay() {
+  Super::BeginPlay();
 
-    FindPhysicsHandle();
-    FindInputComponent();
+  FindAndAttachPhysicsHandle();
+  FindAndBindInputComponent();
 }
 
-void UGrabber::FindPhysicsHandle() {
+void UGrabber::FindAndAttachPhysicsHandle() {
     /// Look for attached Physics Handle
     PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 
-    if (PhysicsHandle) {
-        UE_LOG(LogTemp, Warning, TEXT("Physics handle found!"))
-
-    } else {
+    if (!PhysicsHandle) {
         UE_LOG(LogTemp, Error, TEXT("Cannot find PhysicsHandle in object = %s"), *(GetOwner()->GetName()))
     }
 }
 
-void UGrabber::FindInputComponent() {
+void UGrabber::FindAndBindInputComponent() {
     // Look for attached InputComponent (only appears in runtime)
     InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
 
     if (InputComponent) {
-        UE_LOG(LogTemp, Warning, TEXT("Input component found!"))
-
         /// Bind the input axis
         InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
         InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
@@ -101,6 +76,22 @@ void UGrabber::FindInputComponent() {
 
 const FHitResult UGrabber::GetPhysicsBodyInReach() {
 
+    MWFGrabVector GrabVector = GetGrabVector();
+
+    /// Line-trace out to reach distance
+    FHitResult Hit;
+    GetWorld()->LineTraceSingleByObjectType(
+                OUT Hit,
+                std::get<0>(GrabVector),
+                std::get<1>(GrabVector),
+                FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+                FCollisionQueryParams(FName(TEXT("")), false, GetOwner())
+                );
+
+    return Hit;
+}
+
+const MWFGrabVector UGrabber::GetGrabVector() const {
     FVector PlayerViewLocation;
     FRotator PlayerViewRotation;
 
@@ -111,22 +102,6 @@ const FHitResult UGrabber::GetPhysicsBodyInReach() {
 
     FVector LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * Reach;
 
-    /// Line-trace out to reach distance
-    FHitResult Hit;
-    FCollisionQueryParams QueryParams = FCollisionQueryParams(FName(TEXT("")), false, GetOwner());
-
-    GetWorld()->LineTraceSingleByObjectType(
-                OUT Hit,
-                PlayerViewLocation,
-                LineTraceEnd,
-                FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
-                QueryParams);
-
-    /// check what we hit
-    AActor* ActorHit = Hit.GetActor();
-    if (ActorHit) {
-        UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()));
-    }
-    return Hit;
+    return std::make_tuple(PlayerViewLocation, LineTraceEnd);
 }
 
--- Source/Escape/Grabber.h
+++ Source/Escape/Grabber.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <tuple>
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "Engine/World.h"
@@ -12,39 +13,41 @@
 #include "Components/PrimitiveComponent.h"
 #include "Grabber.generated.h"
 
+typedef std::tuple<FVector, FVector> MWFGrabVector;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class ESCAPE_API UGrabber : public UActorComponent
-{
-	GENERATED_BODY()
+class ESCAPE_API UGrabber : public UActorComponent {
+GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UGrabber();
+public:
+  // Sets default values for this component's properties
+  UGrabber();
 
-    // Called every frame
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-    void Grab(); // grab delegate method
-    void Release(); // release delegate method
-
+  // Called every frame
+  virtual void
+  TickComponent(float DeltaTime, ELevelTick TickType,
+                FActorComponentTickFunction *ThisTickFunction) override;
+  void Grab();    // grab delegate method
+  void Release(); // release delegate method
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+  // Called when the game starts
+  virtual void BeginPlay() override;
 
+/// Private fields
 private:
+  // How far player can reach to grab sth
+  float Reach = 100.f;
 
-    // How far player can reach to grab sth
-    float Reach = 100.f;
-	
-    UPhysicsHandleComponent* PhysicsHandle = nullptr;
-    UInputComponent* InputComponent = nullptr;
+  UPhysicsHandleComponent *PhysicsHandle = nullptr;
+  UInputComponent *InputComponent = nullptr;
 
+/// Private methods
 private:
+  void FindAndAttachPhysicsHandle();
+  void FindAndBindInputComponent();
 
-    void FindPhysicsHandle();
-    void FindInputComponent();
-
-    // Return hit for first physics body in reach
-    const FHitResult GetPhysicsBodyInReach();
+  // Return hit for first physics body in reach
+  const FHitResult GetPhysicsBodyInReach();
+  const MWFGrabVector GetGrabVector() const;
 };
